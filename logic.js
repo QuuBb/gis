a961ae5c268ff0c05d2613382f6ed28f572dd142
QuuBb <k.surdel@o2.pl>
2023-10-27 15:50:17 +0200

    Info popup added

--- logic.js
+++ logic.js
@@ -4,14 +4,17 @@ import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 import {useGeographic} from 'ol/proj';
-import {Feature} from 'ol';
+import {Feature, Overlay} from 'ol';
 import {Point} from 'ol/geom';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
+import {fromLonLat} from 'ol/proj';
+import {add} from 'ol/coordinate';
 
 const list = document.getElementsByTagName('ul')[0];
+const popupInfo = document.getElementById('popupInfo');
 
 const defLat = 54.4;
 const defLong = 18.6;
@@ -63,7 +66,7 @@ class App {
     RenderPointsToMap() {
         this.points.forEach(point => {
             let pointRender = new Feature({
-                geometry: new Point([point.latitude, point.longitude]),
+                geometry: new Point([point.longitude, point.latitude]),
                 name: point.name,
             });
 
@@ -95,10 +98,9 @@ class App {
 
     RenderPointToList() {
         this.points.forEach((point, i) => {
-            console.log('ebe');
             list.insertAdjacentHTML(
                 'beforeend',
-                `<li data=${i} class="border-b-2 border-black 80 flex items-center">
+                `<li class="border-b-2 border-black 80 flex items-center cursor-pointer">
                     <div class="w-full flex items-center p-1">
                     <div class="w-8 h-8">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -112,12 +114,54 @@ class App {
                     </div>
                 </li>`
             );
+
+            list.lastElementChild.addEventListener('click', () => {
+                this.ShowPointInfo(point);
+                this.SetViewToPoint(point.longitude, point.latitude);
+            });
         });
     }
 
     RenderPoints() {
         this.RenderPointToList();
         this.RenderPointsToMap();
+    }
+
+    SetViewToPoint(long, lat) {
+        const cords = [long, lat];
+        add(cords, [10, 15]);
+        this.view.setCenter(cords);
+        this.view.setZoom(18);
+    }
+
+    ShowPointInfo(point) {
+        const x = this.map.getOverlays().getArray()[0];
+        if (x !== undefined) this.map.removeOverlay(x);
+        console.log(this.map);
+        console.log(x);
+        this.map.removeOverlay(0);
+        const overlay = new Overlay({
+            element: popupInfo,
+            autoPan: {
+                animation: {
+                    duration: 250,
+                },
+            },
+            positioning: 'bottom-center',
+            offset: [0, -60],
+        });
+
+        this.map.addOverlay(overlay);
+
+        console.log('ebe');
+
+        overlay.setPosition([point.longitude, point.latitude]);
+
+        popupInfo.getElementsByTagName('img')[0].src = point.photo;
+        popupInfo.getElementsByTagName('h4')[0].innerHTML = point.name;
+        popupInfo.getElementsByTagName('p')[2].innerHTML = point.description;
+
+        popupInfo.classList.remove('hidden');
     }
 }
 
