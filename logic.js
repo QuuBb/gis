dd56814a909a2d30db9dc38e052297cb1b0cc183
QuuBb <k.surdel@o2.pl>
2023-10-31 12:13:42 +0100

    delete icon

--- logic.js
+++ logic.js
@@ -89,13 +89,19 @@ class App {
     }
 
     LoadDefaultPoints() {
-        this.points.push(...data.monuments);
+        const points = [...data.monuments];
+        points.forEach(point => {
+            point.default = true;
+        });
+        this.points.push(...points);
     }
 
     LoadSavedPoints() {
         if (localStorage.length == 0) return;
         for (let i = 0; i < localStorage.length; i++) {
-            this.points.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            const point = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            point.default = false;
+            this.points.push(point);
         }
     }
 
@@ -141,22 +147,44 @@ class App {
     RenderPointToList() {
         list.innerHTML = '';
         this.points.forEach((point, i) => {
-            list.insertAdjacentHTML(
-                'beforeend',
-                `<li class="border-b-2 border-black 80 flex items-center cursor-pointer">
-                    <div class="w-full flex items-center p-1">
-                    <div class="w-8 h-8">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-8 h-8 min-w-[32 rem]">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                        </svg>
-                    </div>
-                    <h3 class="text-xl">${point.name}</h3>
-                    </div>
-                </li>`
-            );
+            let liHTML;
+            if (point.default) {
+                liHTML = `<li class="border-b-2 border-black 80 flex items-center cursor-pointer">
+                <div class="w-full flex items-center p-1">
+                <div class="w-8 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-8 h-8 min-w-[32 rem]">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                    </svg>
+                </div>
+                <h3 class="text-xl">${point.name}</h3>
+                </div>
+            </li>`;
+            } else {
+                liHTML = `<li class="border-b-2 border-black 80 flex items-center cursor-pointer">
+                <div class="w-full flex items-center p-1">
+                <div class="w-8 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-8 h-8 min-w-[32 rem]">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                    </svg>
+                </div>
+                <h3 class="text-xl">${point.name}</h3>
+                <button class="w-8 h-8 ml-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+              
+                </button>
+                </div>
+            </li>`;
+            }
+
+            list.insertAdjacentHTML('beforeend', liHTML);
 
             list.lastElementChild.addEventListener('click', () => {
                 this.ShowPointInfo(point);
